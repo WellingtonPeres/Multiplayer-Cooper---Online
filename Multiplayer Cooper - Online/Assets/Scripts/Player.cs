4bae22c9f6037f53a057dfecee0d7f2bc009c70b
Wellington Peres <wellingtonpv.100@gmail.com>
2023-02-02 18:42:28 -0300

    Adicionei o player na cena e fiz o player olhar na direção que está andando e quando ele para, continua olhando para direção ao qual parou

--- Multiplayer Cooper - Online/Assets/Scripts/Player.cs
+++ Multiplayer Cooper - Online/Assets/Scripts/Player.cs
@@ -31,6 +31,8 @@ public class Player : MonoBehaviour
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        Debug.Log(inputVector);
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
 }
