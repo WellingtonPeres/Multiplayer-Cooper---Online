c459710a2bb17d18c8a5f2b26eb403c4ac8b39eb
Wellington Peres <wellingtonpv.100@gmail.com>
2023-02-03 08:47:43 -0300

    Usei eventos para pegar a interação com objetos (ClearCounters) até o momento

--- Multiplayer Cooper - Online/Assets/Scripts/Player.cs
+++ Multiplayer Cooper - Online/Assets/Scripts/Player.cs
@@ -11,6 +11,34 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDirection;
 
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDirection != Vector3.zero)
+        {
+            lastInteractDirection = moveDirection;
+        }
+
+        float interactDisctance = 2f;
+        RaycastHit raycastHit;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out raycastHit, interactDisctance))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
     private void Update()
     {
         HandleMovement();
@@ -35,12 +63,11 @@ public class Player : MonoBehaviour
 
         float interactDisctance = 2f;
         RaycastHit raycastHit;
-        if (Physics.Raycast(transform.position, lastInteractDirection, out raycastHit, interactDisctance, countersLayerMask))
+        if (Physics.Raycast(transform.position, lastInteractDirection, out raycastHit, interactDisctance))
         {
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 // Has ClearCounter
-                clearCounter.Interact();
             }
         }
     }
