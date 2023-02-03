51d868e11f35a3cbd2b80ff8e89cf577c6b7a86f
Wellington Peres <wellingtonpv.100@gmail.com>
2023-02-03 17:52:22 -0300

    Fiz o item trocar de mesa ao clicar novamente no item para ter espaço na mesma mesa para poder pegar outro item

--- Multiplayer Cooper - Online/Assets/Scripts/ClearCounter.cs
+++ Multiplayer Cooper - Online/Assets/Scripts/ClearCounter.cs
@@ -6,13 +6,58 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private kitchenObjectScriptableObject kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing; 
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
-        Debug.Log("Interact!");
-        Transform kitchenObjetcTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjetcTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjetcTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjetcTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            kitchenObjetcTransform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjetcTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTrnasform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+    
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Multiplayer Cooper - Online/Assets/Scripts/KitchenObject.cs
+++ Multiplayer Cooper - Online/Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,35 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private kitchenObjectScriptableObject kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public kitchenObjectScriptableObject GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("counter already has a kitchenObject!");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTrnasform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
