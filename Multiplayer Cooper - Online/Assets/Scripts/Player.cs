8559c7bab1f11884716218e2611438fb1c5c7326
Wellington Peres <wellingtonpv.100@gmail.com>
2023-02-03 07:52:57 -0300

    Coloquei Detecção de colisão visa script

--- Multiplayer Cooper - Online/Assets/Scripts/Player.cs
+++ Multiplayer Cooper - Online/Assets/Scripts/Player.cs
@@ -14,7 +14,49 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        
+        if (!canMove)
+        {
+            // Cannot move towards moveDirection
+
+            // Attempt only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            {
+                // Can move only on the X
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove)
+                {
+                    //Can move only on the Z
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    // CAnnot move in any direction
+                }
+            }
+        }
+        
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
